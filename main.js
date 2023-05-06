39529fbe0de19ccf63aa4a76c78a16ec331cde6b
shakosauri219 <shakosauri214@gmail.com>
2023-05-07 01:40:04 +0400

    full.js

--- main.js
+++ main.js
@@ -2,22 +2,46 @@ const bill = document.getElementById('bill-input');
 const numberPeople = document.getElementById('number-people');
 const errorText = document.getElementById('error-text');
 const custom = document.getElementById('custom-percent');
+const totalPerson = document.getElementById('total');
+const tipPerson = document.getElementById('tip');
+const button = document.getElementById('reset');
+
+
 function onClickPrecent(percent) {
-    const totalPerson = document.getElementById('total');
-    const tipPerson = document.getElementById('tip');
-    let total = parseInt(bill.value) / parseInt(numberPeople.value);
-    let tip = parseFloat((percent / parseInt(bill.value)) * 100);
-    let totalBill = total + tip;
-    if(bill.value == "" && numberPeople.value == "0"){
+    if(numberPeople.value == ""){
         errorText.style.display = "block";
     }else if(numberPeople.value >= "1" && custom.value == ""){
-        totalPerson.innerHTML = "$" + totalBill.toFixed(2);
-        tipPerson.innerHTML = "$" + tip.toFixed(2);
-    }else if(numberPeople.value >= "1" && custom.value !== ""){
-        totalPerson.innerHTML = "$" + totalBill.toFixed(2);
-        tipPerson.innerHTML = "$" + tip.toFixed(2);    
+        let tip = (percent / parseInt(bill.value)) * 100 / parseInt(numberPeople.value);
+        let total = ((parseInt(bill.value) / parseInt(numberPeople.value)) + tip);
+        totalPerson.innerHTML = "$" + parseFloat(total.toFixed(2));
+        tipPerson.innerHTML = "$" + parseFloat(tip.toFixed(2));
     }
 }
+
+
+custom.addEventListener('input', () => {
+    if(numberPeople.value == ""){
+        errorText.style.display = 'block';
+    }else if(numberPeople.value !== ""){
+        errorText.style.display = 'none';
+    }
+    if(numberPeople.value >= "1" && custom.value >= "1"){
+        let tip = (custom.value / parseInt(bill.value)) * 100 / parseInt(numberPeople.value);
+        let total = ((parseInt(bill.value) / parseInt(numberPeople.value)) + tip);
+        totalPerson.innerHTML = "$" + parseFloat(total.toFixed(2));
+        tipPerson.innerHTML = "$" + parseFloat(tip.toFixed(2));
+    }
+});
+
+
+button.addEventListener('click', () => {
+    bill.value = "";
+    custom.value = "";
+    numberPeople.value = "";
+    totalPerson.innerHTML = "$0.00";
+    tipPerson.innerHTML = "$0.00";
+});
+
 
 
 
